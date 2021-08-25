170da3251b5e25a5b57f37ea25a9ee5747d699ca
Philip Plagge <plaggep@gmail.com>
2021-08-25 19:22:21 +0200

    Initial changes

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+const IndexPage = () => {
+  return (
+    <main>
+      <title>itscursedphil</title>
+      <h1>Test</h1>
+    </main>
+  );
+};
+
+export default IndexPage;
